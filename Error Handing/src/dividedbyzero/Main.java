703081f703530cabefff3456a6a95434e75122c4
Alcibiade <1131069793@qq.com>
2024-03-20 23:36:11 +0800

    try/catch block added

---
+++ Error Handing/src/dividedbyzero/Main.java
@@ -0,0 +1,11 @@
+package dividedbyzero;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println(3 / 0);
+        } catch (ArithmeticException e) {
+            System.out.printf("Caught runtime exception = %s%n", e.getMessage());
+        }
+    }
+}
